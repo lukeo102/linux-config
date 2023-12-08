a66704fb0882424eb0ffd35ce6f7ed92cdecae7a
luke102 <lukeormiston@gmail.com>
2023-12-08 19:16:18 +0000

    init

---
+++ commands.h
@@ -0,0 +1,5 @@
+
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "kitty", NULL };
+static const char *browser[] = { "firefox", NULL };
+
