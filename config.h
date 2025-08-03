772ced9afbae62f1c50c5aa4648a2a6aa286a1f2
luke <lukeormiston@gmail.com>
2025-08-03 15:20:22 +0100

    discord n steam on their own tags

--- config.h
+++ config.h
@@ -39,8 +39,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+  { "discord",  "discord",  NULL,       1,            0,            1 }, 
+  { "steam",    NULL,       NULL,       1 << 4,       0,            0 },
 };
 
 /* layout(s) */
